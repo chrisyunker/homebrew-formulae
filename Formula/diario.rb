2a4ef6de4dbc4db98038133d2797ad0cda311563
Chris Yunker <chrisyunker@gmail.com>
2018-06-02 14:20:09 -0500

    Update diario for v0.2.1

--- Formula/diario.rb
+++ Formula/diario.rb
@@ -1,9 +1,9 @@
 class Diario < Formula
   desc "Daily log tool"
   homepage "https://github.com/chrisyunker/diario"
-  url "https://github.com/chrisyunker/diario/archive/v0.2.0.tar.gz"
-  sha256 "c69e077e97cbe0b050394906b12082dc4d804fec02396e44e59ca2adabc6c7a2"
-  version "0.2.0"
+  url "https://github.com/chrisyunker/diario/archive/v0.2.1.tar.gz"
+  sha256 "e23f6bc1767808dfb88f3529396e3e0e5518fa341df2daff7845ae0a06ac64c8"
+  version "0.2.1"
 
   bottle :unneeded
 
