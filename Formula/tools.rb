283c074270520c90215394c33742f203471a6a0f
Chris Yunker <chrisyunker@gmail.com>
2021-10-19 12:31:39 -0500

    Try fix bottle change for tools

--- Formula/tools.rb
+++ Formula/tools.rb
@@ -2,10 +2,11 @@ class Tools < Formula
   desc "Collection of command line tools"
   homepage "https://github.com/chrisyunker/tools"
   url "https://github.com/chrisyunker/tools/archive/v0.2.0.tar.gz"
-  sha256 "5a5cd877b5e6d3df7ab95c9229c68989096cb45140f4d4249e3fa89778e8d4f5"
-  version "0.2.0"
+  version "0.2.1"
 
-  bottle :unneeded
+  bottle do
+    sha256 catalina: "5a5cd877b5e6d3df7ab95c9229c68989096cb45140f4d4249e3fa89778e8d4f5"
+  end
 
   def install
     bin.install "toggle-dock"
